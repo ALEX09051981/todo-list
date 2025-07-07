ba411a59e74ca1361a17cee3e0569193fc7b8814
Alexey <alopar632@gmail.com>
2025-07-07 10:40:24 +0300

    Refactor views to use class-based views

--- todo/urls.py
+++ todo/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
-from . import views
+from .views import (
+    HomeView,
+    TaskToggleStatusView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('tags/', views.tag_list, name='tag-list'),
-    path('task/add/', views.task_create, name='task-add'),
-    path('task/<int:pk>/update/', views.task_update, name='task-update'),
-    path('task/<int:pk>/delete/', views.task_delete, name='task-delete'),
-    path('task/<int:pk>/toggle/', views.toggle_task, name='task-toggle'),
-    path('tag/add/', views.tag_create, name='tag-add'),
-    path('tag/<int:pk>/update/', views.tag_update, name='tag-update'),
-    path('tag/<int:pk>/delete/', views.tag_delete, name='tag-delete'),
+    path("", HomeView.as_view(), name="home"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/toggle/", TaskToggleStatusView.as_view(), name="task-toggle"),
+
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
